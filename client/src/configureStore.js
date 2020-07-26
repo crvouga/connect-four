739d155208d1e6b732383f33054ce1a4bc06c899
Christopher Vouga <crvouga@gmail.com>
2020-07-26 14:08:30 -0700

    🧰always expose dev tools

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -17,7 +17,6 @@ const persistedReducer = persistReducer(persistConfig, reducer);
 export default () => {
   const store = configureStore({
     reducer: persistedReducer,
-    devTools: process.env.NODE_ENV !== "production",
     middleware: [sagaMiddleware],
   });
   const persistor = persistStore(store);
